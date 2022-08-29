98a4d1d89780e1aedf9c04a9961c176ff53126c9
Ethical Hacking LK <105837208+EthicalHackingLK@users.noreply.github.com>
2022-08-29 20:40:51 +0530

    Add files via upload

---
+++ thank2.sh
@@ -0,0 +1,3 @@
+z="
+";Yz='║░██';qz='░╚═╝';oz='░░░╚';vz='═╝';dz='██╗█';fz='╝░';Xz='█║██';Ez=' -e ';jz='╔═██';uz='╝░░╚';tz='═╝╚═';bz='█║░░';Iz='╗██╗';pz='═╝░░';Fz='"${g';Zz='╔╝';Dz='echo';Hz='████';hz='║██║';cz='║███';Kz='╗░██';nz='║░╚█';Az='g="\';sz='╝╚═╝';Rz='█╔══';Cz=';1m"';lz='░██║';az='░░░█';Tz='║██╔';ez='███═';Oz='█╗██';Mz='░███';Wz='█╗░█';Sz='╝██║';Uz='══██';gz='░██╔';mz='░╚██';Vz='╗███';iz='╚███';Gz='}';kz='╗░';Jz='░░██';Lz='███╗';Qz='╚══█';Pz='█╗';rz='░░╚═';Bz='e[33';Nz='╗░░█';wz='"';
+eval "$Az$Bz$Cz$z$Dz$Ez$Fz$Gz$z$Hz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Nz$Pz$z$Qz$Rz$Sz$Jz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$z$az$bz$Mz$Hz$cz$Hz$Tz$dz$Xz$ez$fz$z$az$bz$gz$Uz$Tz$Uz$hz$iz$Xz$jz$kz$z$az$bz$lz$Jz$hz$Jz$hz$mz$Xz$nz$Pz$z$oz$pz$qz$rz$sz$rz$sz$rz$tz$uz$vz$z$wz"
